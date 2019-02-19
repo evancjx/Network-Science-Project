dd90039a8476db91a15ffe8a84f8c47f108b428b
Evan <evancjx@gmail.com>
2019-02-19 16:42:02 +0800

    Project version 0.2 1. After multiple version found a stable one 2. Able to compute degree distribution

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import os
 import time
+import collections
 
 import config as CONFIG
 import graphCreators.graphCreator as GRAPH_CREATOR
@@ -22,7 +23,9 @@ def compute_real_network_properties():
     # number of edge
 
 
-def load_graph(name):
+# Where node_id not in txt means not connected at all, standalone node
+# WRONG!
+def load_graph_v1(name):
     # f = open(os.path.join(CONFIG.NETWORKS_DIR, name + '.txt'), 'r')
     start_time = int(round(time.time() * 1000))
 
@@ -32,18 +35,108 @@ def load_graph(name):
         # print(f_contents)
 
         # empty list
-        vertex = list()
-        edgeto = list()
+        network = dict()
+        edge_to_list = list()
+        node_id = 1
+        # count_edge = 0
         for line in f:
             if line.startswith('#'):
                 continue
             split = line.split()
-            vertex.append(split[0])
-            edgeto.append(split[1])
+            while int(split[0]) != node_id:
+                network[node_id] = edge_to_list
+                edge_to_list = list()
+                node_id += 1
+
+            if int(split[0]) == node_id:
+                edge_to_list.append(split[1])
+            # count_edge += 1
+        # last line
+        network[node_id] = edge_to_list
+        # network[0] = count_edge
 
     end_time = int(round(time.time() * 1000))
     compute_time = int(end_time) - int(start_time)
     print('({}ms) Read graph from {} txt file'.format(compute_time, name))
-    degree_analyzer.count_degree(vertex, vertex)
+    return network
 
-load_graph('com-amazon.ungraph')
+# Did not account to end nodes
+# in txt file, it is about edges, start node to end node
+def load_graph_v2(name):
+    # f = open(os.path.join(CONFIG.NETWORKS_DIR, name + '.txt'), 'r')
+    start_time = int(round(time.time() * 1000))
+
+    # context manager
+    with open(os.path.join(CONFIG.NETWORKS_DIR, name + '.txt'), 'r') as f:
+        # f_contents = f.readline()
+        # print(f_contents)
+
+        # empty list
+        network = dict()
+        edge_to_list = list()
+        current_node = 1
+        last_node = 1
+        # count_edge = 0
+        for line in f:
+            if line.startswith('#'):
+                continue
+
+            split = line.split()
+            # if int(split[0]) < 540000:
+            #     continue
+            # if int(split[0]) > 541000:
+            #     break
+
+            if int(split[0]) == last_node:
+                edge_to_list.append(split[1])
+            elif current_node != int(split[0]):
+                network[last_node] = edge_to_list
+                edge_to_list = list()
+                current_node = int(split[0])
+                edge_to_list.append(split[1])
+                last_node = current_node
+
+        network[current_node] = edge_to_list  # last line
+
+    end_time = int(round(time.time() * 1000))
+    compute_time = int(end_time) - int(start_time)
+    print('({}ms) Read graph from {} txt file'.format(compute_time, name))
+    return network
+
+
+def load_graph_v3(name):
+    # f = open(os.path.join(CONFIG.NETWORKS_DIR, name + '.txt'), 'r')
+    start_time = int(round(time.time() * 1000))
+    count = 0
+    # context manager
+    with open(os.path.join(CONFIG.NETWORKS_DIR, name + '.txt'), 'r') as f:
+        network = dict()
+        for line in f:
+            if line.startswith('#'):
+                continue
+
+            count += 1
+            split = line.split()
+
+            if int(split[0]) not in network:
+                edge_to_list = [split[1]]
+                network[int(split[0])] = edge_to_list
+            elif int(split[0]) in network:
+                # print(list(network[int(split[0])]))
+                edge_to_list = list(network[int(split[0])])
+                edge_to_list.append(split[1])
+                network[int(split[0])] = edge_to_list
+    s_network = dict()
+    for key in sorted(network.keys()):
+        s_network[key] = network[key]
+    end_time = int(round(time.time() * 1000))
+    compute_time = int(end_time) - int(start_time)
+    print('({}ms) Read graph from {} txt file'.format(compute_time, name))
+    return s_network
+
+
+network = load_graph_v3('com-amazon.ungraph')
+# print(network)
+print('size of the network: ' + str(len(network)))
+# print(network[380262])
+print(degree_analyzer.degree_distribution(network))
--- graph/analyzer/degree_analyzer.py
+++ graph/analyzer/degree_analyzer.py
@@ -1,7 +1,8 @@
 from itertools import groupby
 import time
 
-def count_degree(vertex, egdeto):
+
+def count_degree_old(vertex):
     # not a good way to get degree
     # each_node_degree = [len(list(group)) for key, group in groupby(vertex)]
     # print(len(each_node_degree))
@@ -29,10 +30,43 @@ def count_degree(vertex, egdeto):
     compute_time = int(end_time) - int(start_time)
     print('({}ms) Count degree'.format(compute_time))
 
-    # for index, degree in enumerate(each_node_degree):
-    #     # print(key, degree)
-    #     print(index, degree)
-    #     if index == 100:
-    #         break
-
     return each_node_degree
+
+
+def count_degree(network):
+    start_time = int(round(time.time() * 1000))
+    node_degree = {}
+    for key, value in network.items():
+        node_degree[key] = len(value)
+        # if key == 10:
+        #     break
+    end_time = int(round(time.time() * 1000))
+    compute_time = int(end_time) - int(start_time)
+    print('({}ms) Count degree'.format(compute_time))
+    return node_degree
+
+
+def count_node_with_degree_x(node_degree):
+    degree_frequency = {}
+    for key, value in node_degree.items():
+        if value not in degree_frequency:
+            degree_frequency[value] = 0
+        degree_frequency[value] += 1
+    s_degree_frequency = dict()
+    for key in sorted(degree_frequency.keys()):
+        s_degree_frequency[key] = degree_frequency[key]
+    return s_degree_frequency
+
+
+def calculate_degree_distribution(no_of_nodes, degree_frequency):
+    prob_degree_distribution = {}
+    for key, value in degree_frequency.items():
+        prob_degree_distribution[key] = float(value / no_of_nodes)
+    return prob_degree_distribution
+
+
+def degree_distribution(network):
+    node_degree = count_degree(network)
+    degree_frequency = count_node_with_degree_x(node_degree)
+    return calculate_degree_distribution(len(network), degree_frequency)
+
